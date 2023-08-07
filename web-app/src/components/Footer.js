8207fe2e06a7e36c2b03cd5d905df096b7a21cea
Fikewa Olatunji <fikewabdul@gmail.com>
2023-08-07 05:27:58 +0100

    test

---
+++ web-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import footer_logo from '../assets/logo_footer.png'
+
+function Footer() {
+    return (
+      <>
+      <img src={footer_logo} alt=''/>
+  </>
+    );
+  }
+
+  export default Footer;
